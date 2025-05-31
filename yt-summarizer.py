9273a466256809fde96e8e419564a72a03c8950f
Alex Liu <mingzhangliu@gmail.com>
2025-05-31 13:33:06 +0800

    Added Chinese to transcript language

--- yt-summarizer.py
+++ yt-summarizer.py
@@ -1,5 +1,5 @@
 from youtube_transcript_api import YouTubeTranscriptApi
 
 def get_youtube_transcript(video_id):
-    transcript = YouTubeTranscriptApi.get_transcript(video_id)
+    transcript = YouTubeTranscriptApi.get_transcript(video_id, languages=['en', 'zh-Hant', 'zh-TW', 'zh-CN'])
     return ' '.join([entry['text'] for entry in transcript])
